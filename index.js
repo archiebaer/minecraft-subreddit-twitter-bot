0659ba0a638ad8ddebedd8e6c6e7bdde69cae2a7
Archie Baer <archie@abaer.dev>
2019-10-21 00:32:20 +0100

    Gets reddit posts that have an image from the last 12 hours

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const twitter = require("twit");
 const fs = require("fs");
 const credentials = JSON.parse(fs.readFileSync(__dirname + "/credentials.json", "utf8"));
 const t = new twitter(credentials.twitter);
+const request = require("request");
 
 function tweet(status) {
   t.post("statuses/update", {status}, (err, data, response) => {
@@ -9,4 +10,30 @@ function tweet(status) {
   });
 }
 
-tweet("Hello World...");
+function getPosts() {
+  return new Promise((resolve, reject) => {
+    request("https://www.reddit.com/r/minecraft/hot.json?sort=new", (err, res, body) => {
+      if (err) return reject(err);
+      if (res.statusCode != 200) return reject("Status Code " + res.statusCode + " recieved");
+      var posts = JSON.parse(body).data.children.map((p) => {
+        return {
+          title: p.data.title,
+          score: p.data.score,
+          postUrl: `https://reddit.com/r/${p.data.subreddit}/comments/${p.data.id}`,
+          url: p.data.url,
+          isImage: p.data.url.startsWith("https://i.redd.it/"),
+          date: new Date(p.data.created_utc * 1000)
+        };
+      });
+
+      posts = posts.filter(p => {
+        return p.isImage &&
+        (new Date().getTime() - p.date.getTime() < 1000 * 60 * 60 * 12);
+      });
+
+      console.log(posts);
+    });
+  });
+}
+
+getPosts().catch(() => {});
